cd14b491ce79c17f9d17089f244f01cfcc60fb9b
ct-greyson <greysonf@codingtemple.com>
2024-07-12 11:38:30 -0400

    add backend!!

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FeatureComponent from './components/FeatureComponent'
+import BackendComponent from './components/BackendComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <FeatureComponent />
+      <BackendComponent />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
