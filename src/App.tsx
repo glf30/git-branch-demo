06d2f3dcccb384c19bb753e4013d6903f6526f6c
ct-greyson <greysonf@codingtemple.com>
2024-07-12 12:10:17 -0400

    new component stuff!

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import FeatureComponent from './components/FeatureComponent'
 import BackendComponent from './components/BackendComponent'
+import CTComponent from './components/CTComponent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <BackendComponent />
+      <CTComponent />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
