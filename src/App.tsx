e5cb23812faf92e02bbb670285ca8b7d68e77bb5
ct-greyson <greysonf@codingtemple.com>
2024-07-12 11:51:29 -0400

    new title

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>React</h1>
+      <h1>New title</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
